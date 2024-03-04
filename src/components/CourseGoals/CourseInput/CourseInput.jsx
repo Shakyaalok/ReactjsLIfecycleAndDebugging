5b2c55c93170b753d20522107706cbae879eb74d
Shakyaalok <108414896+Shakyaalok@users.noreply.github.com>
2024-03-04 13:17:42 +0530

    useRef to the form instead of state

--- src/components/CourseGoals/CourseInput/CourseInput.jsx
+++ src/components/CourseGoals/CourseInput/CourseInput.jsx
@@ -1,33 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState,useRef } from 'react';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
 const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+  // const [enteredValue, setEnteredValue] = useState('');
   const [isValid,setIsValid] = useState(true)
 
+  const enterdRef = useRef();
+
   const goalInputChangeHandler = event => {
-    if(enteredValue.trim().length>0){
+    if(enterdRef.current.value.trim().length>0){
       setIsValid(true);
     }
-    setEnteredValue(event.target.value);
+    
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
-    if(enteredValue.trim().length===0){
+    console.log(enterdRef)
+    if(enterdRef.current.value.trim().length===0){
       setIsValid(false);
       return;
     }
-    props.onAddGoal(enteredValue);
+    props.onAddGoal(enterdRef.current.value);
+    enterdRef.current.value='';
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className={`form-control ${!isValid ? 'invalid':''}`}>
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input type="text" onChange={goalInputChangeHandler} ref={enterdRef}/>
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
