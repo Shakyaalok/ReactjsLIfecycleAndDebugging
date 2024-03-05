85475ff36f431857ce8b55ac06fd845abf02dd6c
Shakyaalok <108414896+Shakyaalok@users.noreply.github.com>
2024-03-05 14:01:22 +0530

    reducer in email

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,19 +1,32 @@
-import React, { useState,useEffect} from 'react';
+import React, { useState,useEffect,useReducer} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+const emailReducer = (state,action) =>{
+  if(action.type==="USER_INPUT"){
+    return {value:action.val, isValid:action.val.includes('@')};
+  }
+  if(action.type==='INPUT_BLUR'){
+    return {value:state.value,isValid:state.value.includes('@')};
+  }
+  return {value:'', isValid:false};
+}
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [enteredCollege,setEnteredCollege]  = useState('')
   const [collegeIsValid, setCollegeIsValid] = useState();
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  const [emailState,dispactedEmail] = useReducer(emailReducer,{
+    value:'',
+    isValid:false
+   })
 
+/*
   useEffect(()=>{
   const identifier =  setTimeout(()=>{
     console.log('checking form validity!')
@@ -28,32 +41,45 @@ const Login = (props) => {
    }
   },[enteredEmail,enteredPassword,enteredCollege])  // we re-render our componenet when email and password changes
   // but still there is a problem like useEffect calls repeatedly for every key strokes we can do such that useTImers
+*/
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
-
+    dispactedEmail({type:'USER_INPUT',val:event.target.value})
+    setFormIsValid(
+      event.target.value.includes('@') && enteredPassword.trim().length > 6 && enteredCollege.trim().length>3
+    );
    
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
+    setFormIsValid(
+      emailState.isValid && event.target.value.trim().length > 6 && enteredCollege.trim().length>3
+    );
   };
 
   const collegeChangeHandler = (event)=>{
-    setEnteredCollege(event.target.value)
+    setEnteredCollege(event.target.value);
+    setFormIsValid(
+      emailState.isValid && enteredPassword.trim().length > 6 && event.target.value.trim().length>3
+    );
   }
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    dispactedEmail({type:'INPUT_BLUR'})
   };
 
   const validatePasswordHandler = () => {
     setPasswordIsValid(enteredPassword.trim().length > 6);
   };
 
+  const validateCollegeHandler = () =>{
+    setCollegeIsValid(enteredCollege.trim().length > 3);
+  }
+
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -61,14 +87,14 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
@@ -98,6 +124,7 @@ const Login = (props) => {
             id="college"
             value={enteredCollege}
             onChange={collegeChangeHandler}
+            onBlur={validateCollegeHandler}
           />
         </div>
         <div className={classes.actions}>
