8f0e0dfaeebcfc2644226e6a55d5c2418765ad8a
Shakyaalok <108414896+Shakyaalok@users.noreply.github.com>
2024-03-13 12:54:19 +0530

    auth context pattern

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,9 @@
-import React, { useState,useEffect,useReducer} from 'react';
+import React, { useState,useEffect,useReducer,useContext} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context'
 
 const emailReducer = (state,action) =>{
   if(action.type==="USER_INPUT"){
@@ -25,6 +26,8 @@ const Login = (props) => {
     value:'',
     isValid:false
    })
+
+   const authCtx = useContext(AuthContext)
 
 /*
   useEffect(()=>{
@@ -79,7 +82,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, enteredPassword);
+    authCtx.onLogin(emailState.value, enteredPassword);
   };
 
   return (
